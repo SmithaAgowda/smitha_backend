d43390ef426f7249f59355b2aac29bdc5f9be782
Smitha M G. <smitha.mg@excelindia.com>
2024-05-09 18:17:55 +0530

    jdbc

---
+++ Day8/jdbc/src/com/jdbc/student/StudentDatabase.java
@@ -0,0 +1,28 @@
+package com.jdbc.student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+public class StudentDatabase {
+	
+//	private static Connection connection;
+//	private static Scanner scanner = new Scanner(System.in);
+//	
+//	public static void main(String[] args) {
+//		
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//		
+//		String url ="jdbc:mysql://localhost:3306/jdbc";
+//		String user = "root";
+//		String pass = "Sa123";
+//		
+//		Connection connection = DriverManager.getConnection(url,user,pass);
+////		
+//		
+//		System.err.println("press 1 for Inserted");
+//		System.out.println("enter choice");
+//	//	int choice=integer.parseint
+//	}
+
+}
